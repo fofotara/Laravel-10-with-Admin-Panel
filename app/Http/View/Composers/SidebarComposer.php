9556ccf09ee16e397a202a513e72cd4c7a6a9eb8
fofotara <fofotara@gmail.com>
2023-03-07 12:11:58 +0300

    Admin template Complet

---
+++ app/Http/View/Composers/SidebarComposer.php
@@ -0,0 +1,14 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: S.Ahmet BALCI
+	 * Date: 7.03.2023
+	 * Time: 11:46
+	 */
+
+	namespace App\Http\View\Composers;
+
+	class SidebarComposer
+	{
+
+	}
---
+++ app/Main/SidebarPanel.php
@@ -0,0 +1,14 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: S.Ahmet BALCI
+	 * Date: 7.03.2023
+	 * Time: 11:47
+	 */
+
+	namespace App\Main;
+
+	class SidebarPanel
+	{
+
+	}
